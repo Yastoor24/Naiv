1ef7b5e446023d370278cd2a8d07cf114ae8dd12
Yastoor24 <y.alahmadi24@gmail.com>
2019-06-30 11:00:55 +0300

    make the _speed of Red laser appear in unity so we can edit without open the code
    I used [serialzerField]

--- Naiv_game/Assets/Scripts/Enemies/RedLaser.cs
+++ Naiv_game/Assets/Scripts/Enemies/RedLaser.cs
@@ -7,6 +7,8 @@ public class RedLaser : MonoBehaviour
 
     private Animator _anim;
 
+
+    [SerializeField]
     private float _speed = 0.5f;
 
    
@@ -16,6 +18,9 @@ public class RedLaser : MonoBehaviour
     private Vector3 dir;
 
     float distanceThisFrame;
+
+    private float  _slope;              // between player and bullet position to get the degree between them 
+    private float _degree;
 
     void Awake()
     {
@@ -30,6 +35,12 @@ public class RedLaser : MonoBehaviour
         dir = target.position - transform.position;
         distanceThisFrame = 0.05f;
 
+        _slope = (target.position.y - transform.position.y) / (target.position.x - transform.position.x);
+        _degree = Mathf.Atan(_slope);
+
+        transform.RotateAroundLocal(dir, _degree);
+
+   
     }
 
     // Update is called once per frame
