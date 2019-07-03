b90ffc408b6e006792b4ab3cc0548ab85904625a
Faisal Shafiei <f.shafiei@pitt.edu>
2019-07-03 10:31:27 +0300

    Fixed the player's compiler error + Fixed the stretching problem with moving platforms

--- Naiv_game/Assets/Scripts/Player/Player.cs
+++ Naiv_game/Assets/Scripts/Player/Player.cs
@@ -78,117 +78,118 @@ public class Player : MonoBehaviour
             //(original *An existing object that you want to make a copy of*, position *Position for the new object* ,rotation *Orientation of the new object* )
             _bullet = Instantiate(_fireBullet, transform.position, Quaternion.identity);
 
-        if (Input.GetKeyDown(KeyCode.J))
-        {
-               // GameObject bullet = Instantiate(fireBullet, transform.position, Quaternion.identity);
+            if (Input.GetKeyDown(KeyCode.J))
+            {
+                // GameObject bullet = Instantiate(fireBullet, transform.position, Quaternion.identity);
 
                 if (move > 0 || _PlayerSprite.flipX == false)
                 {
-                     _PlayerSprite.flipX = false;
-                 //   bullet.GetComponent<FireBullet>().Speed *= transform.localScale.x;
+                    _PlayerSprite.flipX = false;
+                    //   bullet.GetComponent<FireBullet>().Speed *= transform.localScale.x;
 
                 }
                 else if (move < 0 || _PlayerSprite.flipX == true)
                 {
-                     _PlayerSprite.flipX = true;
-                   // bullet.GetComponent<FireBullet>().Speed *= -transform.localScale.x;
+                    _PlayerSprite.flipX = true;
+                    // bullet.GetComponent<FireBullet>().Speed *= -transform.localScale.x;
                 }
 
 
-        }
-
-
-        _grounded = isGrounded();
-        if (move > 0)
-        {
-            // the player move in right side
-            Flip(true);
-        }
-        else if (move < 0)
-        {
-            //the player move in left side
-            Flip(false);
-        }
-
-
-
-        // if the user pressed on space then will be jump
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded() == true)
-        {
-           // Debug.Log("Jump!");
-            _rigid.velocity = new Vector2(_rigid.velocity.x, _jumpForce);
-
-            _rigid.velocity = new Vector2(_rigid.velocity.x, _jumpForce);
-            StartCoroutine(ResetJumpedRoutine());
-            _PlayerAnim.Jump(true);
-
-        }
-        // speed for the player jump
-        _rigid.velocity = new Vector2(move * _speed, _rigid.velocity.y);
-        _PlayerAnim.Move(move);
-
-
-    }
-
-    bool isGrounded()
-    {
-        // collider grounded and jump
-        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.down, 1f, 1 << 8);
-
-        if (hitInfo.collider != null)
-        {
-            // if the palyer jump false then collider for grounded true
-            if (_resetJumped == false)
-            {
-                //Debug.Log("Grounded");
-                _PlayerAnim.Jump(false);
-                return true;
             }
-        }
-        //Debug.Log("jummmmmmmmmmmmmmmp");
-        return false;
-
-    }
 
 
-    void Flip(bool faceRight)
-    {
-        // when the player flip (faceRight)
-        if (faceRight == true)
+            _grounded = isGrounded();
+            if (move > 0)
+            {
+                // the player move in right side
+                Flip(true);
+            }
+            else if (move < 0)
+            {
+                //the player move in left side
+                Flip(false);
+            }
+
+
+
+            // if the user pressed on space then will be jump
+            if (Input.GetKeyDown(KeyCode.Space) && isGrounded() == true)
+            {
+                // Debug.Log("Jump!");
+                _rigid.velocity = new Vector2(_rigid.velocity.x, _jumpForce);
+
+                _rigid.velocity = new Vector2(_rigid.velocity.x, _jumpForce);
+                StartCoroutine(ResetJumpedRoutine());
+                _PlayerAnim.Jump(true);
+
+            }
+            // speed for the player jump
+            _rigid.velocity = new Vector2(move * _speed, _rigid.velocity.y);
+            _PlayerAnim.Move(move);
+
+
+        } }
+
+        bool isGrounded()
         {
-            _PlayerSprite.flipX = false;
-            _SwordArcSprite.flipX = false;
-            _SwordArcSprite.flipY = false;
-            Vector3 newPos = _SwordArcSprite.transform.localPosition;
-            newPos.x = 1.01f;
-            _SwordArcSprite.transform.localPosition = newPos;
+            // collider grounded and jump
+            RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.down, 1f, 1 << 8);
 
+            if (hitInfo.collider != null)
+            {
+                // if the palyer jump false then collider for grounded true
+                if (_resetJumped == false)
+                {
+                    //Debug.Log("Grounded");
+                    _PlayerAnim.Jump(false);
+                    return true;
+                }
+            }
+            //Debug.Log("jummmmmmmmmmmmmmmp");
+            return false;
 
         }
-        // when the player flip (faceleft)
-        if (faceRight == false)
+
+
+        void Flip(bool faceRight)
         {
-            _PlayerSprite.flipX = true;
-            _SwordArcSprite.flipX = true;
-            _SwordArcSprite.flipY = true;
-            Vector3 newPos = _SwordArcSprite.transform.localPosition;
-            newPos.x = -1.01f;
-            _SwordArcSprite.transform.localPosition = newPos;
+            // when the player flip (faceRight)
+            if (faceRight == true)
+            {
+                _PlayerSprite.flipX = false;
+                _SwordArcSprite.flipX = false;
+                _SwordArcSprite.flipY = false;
+                Vector3 newPos = _SwordArcSprite.transform.localPosition;
+                newPos.x = 1.01f;
+                _SwordArcSprite.transform.localPosition = newPos;
+
+
+            }
+            // when the player flip (faceleft)
+            if (faceRight == false)
+            {
+                _PlayerSprite.flipX = true;
+                _SwordArcSprite.flipX = true;
+                _SwordArcSprite.flipY = true;
+                Vector3 newPos = _SwordArcSprite.transform.localPosition;
+                newPos.x = -1.01f;
+                _SwordArcSprite.transform.localPosition = newPos;
+
+            }
 
         }
 
+        IEnumerator ResetJumpedRoutine()
+        {
+            //activate && Deactivate jump  and wait few seconds between them
+            _resetJumped = true;
+            yield return new WaitForSeconds(0.1f);
+            _resetJumped = false;
+
+        }
+
+
+
+
     }
 
-    IEnumerator ResetJumpedRoutine()
-    {
-        //activate && Deactivate jump  and wait few seconds between them
-        _resetJumped = true;
-        yield return new WaitForSeconds(0.1f);
-        _resetJumped = false;
-
-    }
-
-
-
-
-}
