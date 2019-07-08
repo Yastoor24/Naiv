23625f0ba8ff3e74d4c518348acb560d139b0bea
Wejdan <wejdan.alahmadi94@gmail.com>
2019-07-08 15:49:11 +0300

    added player prefab
    added player prefab to the village house scene

--- Naiv_game/Assets/Scripts/menu/OptionMenu.cs
+++ Naiv_game/Assets/Scripts/menu/OptionMenu.cs
@@ -14,30 +14,6 @@ public class OptionMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (input.GetKeyDown(KeyCode.Escape))
-        {
-            optionMenu.gameObject.SetActive(true);
-
-        }
-    }
-}
-
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class OptionMenu : MonoBehaviour
-{
-    public OptionMenu optionMenu;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             optionMenu.gameObject.SetActive(true);
