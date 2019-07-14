d3abe7c3c220bb2442070c3a28e81b278ae823d0
shaima mousa <shaima.mousa.alharbi@gmail.com>
2019-07-14 16:55:00 +0300

    control mobile by android
    1- download joystick assest for move and jump
    2 - downlaod standard assest for bullet
    3- merge between xbox , pc and control mobile ( android )
    4- run on mobile and transfer between scencs.

--- Naiv_game/Assets/Scripts/Player/Player.cs
+++ Naiv_game/Assets/Scripts/Player/Player.cs
@@ -1,11 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//using UnityStandardAssets.CrossPlatformInput;
+using UnityStandardAssets.CrossPlatformInput;
 public class Player : MonoBehaviour
 {
 
-    private AudioSource _SwordAudio;
+    private AudioSource _GunAudio;
     private Rigidbody2D _rigid;
     [SerializeField]
     private float _jumpForce = 5f;
@@ -15,7 +15,6 @@ public class Player : MonoBehaviour
     [SerializeField]
     private float _speed = 5.0f;
     private SpriteRenderer _PlayerSprite;
-    
     private SpriteRenderer _BulletSprite;
     public static PlayerAnimation _PlayerAnim;
     private bool _grounded = false;
@@ -27,15 +26,12 @@ public class Player : MonoBehaviour
     private GameObject _bullet;
     [SerializeField]
     public Transform HitBox1;
-   
-
+    public Joystick joystick;
+    public float move;
 
     //Awake is used to initialize any variables or game state before the game starts
     void Awake()
-    {
-       
-
-    }
+    {  }
 
     //use his for init
     // Start is called before the first frame update
@@ -44,95 +40,84 @@ public class Player : MonoBehaviour
         _rigid = GetComponent<Rigidbody2D>();
         _PlayerAnim = GetComponent<PlayerAnimation>();
         _PlayerSprite = GetComponentInChildren<SpriteRenderer>();
-      
+        _GunAudio = GetComponent<AudioSource>();
         _anim = GetComponentInChildren<Animator>();
         _BulletSprite = transform.GetChild(1).GetComponent<SpriteRenderer>();
-
-
-
-    }
+  }
 
     // Update is called once per frame
     void Update()
     {
         HitBox1.gameObject.SetActive(false);
-      
+
         if (_canMove)
         {
             //ShootBullet();
             Movement();
+     }
+ }
 
-          
-        }
-
-
-    }
-
-
-
-   public void Movement()
+    public void Movement()
     {
-
+        float verticalMove = joystick.Vertical;
+        move = joystick.Horizontal;
         // player move horizontal (left and right )
-        float move = Input.GetAxisRaw("Horizontal");
+        float move1 = Input.GetAxisRaw("Horizontal");
         _grounded = isGrounded();
 
 
         // if the user pressed on O then will be attack by bullet
-        if (Input.GetKeyDown(KeyCode.J) || Input.GetButton("XboxB") )
+        if (Input.GetKeyDown(KeyCode.J) || Input.GetButton("XboxB") || CrossPlatformInputManager.GetButtonDown("shoot"))
         {
+
             // An existing object that you want to make a copy of, take three thing
             //(original *An existing object that you want to make a copy of*, position *Position for the new object* ,rotation *Orientation of the new object* )
 
             _bullet = Instantiate(_fireBullet, transform.position, Quaternion.identity);
             HitBox1.gameObject.SetActive(true);
+           // _GunAudio.Play();
 
-
-            if (move > 0 || _PlayerSprite.flipX == false)
+            if (move > 0 || move1 > 0 || _PlayerSprite.flipX == false)
             {
                 // when the player in a left side then will be used gun in the left side
 
 
                 _bullet.GetComponent<FireBullet>().Speed *= transform.localScale.x;
                 _bullet.GetComponent<FireBullet>().transform.position = HitBox1.position;
-
-
-
-
+                //_GunAudio.Play();
                 _anim.Play("Shoot");
 
-
-
             }
-            else if (move < 0 || _PlayerSprite.flipX == true)
+            else if (move < 0 || move1 < 0 || _PlayerSprite.flipX == true)
             {
 
-                 _bullet.GetComponent<FireBullet>().Speed *= -transform.localScale.x ;
-               
+                _bullet.GetComponent<FireBullet>().Speed *= -transform.localScale.x;
+
                 Vector3 pos = HitBox1.position;
                 pos.x = -2.7f;
                 pos.y = 0f;
                 HitBox1.position = pos + HitBox1.position;
                 _bullet.GetComponent<FireBullet>().transform.position = HitBox1.position;
-               _bullet.transform.rotation = Quaternion.Euler(0f,0f,-180);
-                _anim.Play("Shoot");}
+                _bullet.transform.rotation = Quaternion.Euler(0f, 0f, -180);
+               // _GunAudio.Play();
 
-
+                _anim.Play("Shoot");
+            }
         }
         // when player just move without any anthor action
-        if (move > 0)
+        if (move > 0 || move1 > 0)
         {
             // the player move in right side
             Flip(true);
         }
-        else if (move < 0)
+        else if (move < 0 || move1 < 0)
         {
             //the player move in left side
             Flip(false);
         }
 
         // if the user pressed on space then will be jump
-        if ((Input.GetKeyDown(KeyCode.Space) || Input.GetButton("XboxA")) && isGrounded() == true)
+        if ((Input.GetKeyDown(KeyCode.Space) || Input.GetButton("XboxA") || verticalMove >= 0.5f) && isGrounded() == true)
         {
 
             _rigid.velocity = new Vector2(_rigid.velocity.x, _jumpForce);
@@ -141,20 +126,16 @@ public class Player : MonoBehaviour
 
         }
         // speed for the player jump
-        _rigid.velocity = new Vector2(move * _speed, _rigid.velocity.y);
-        _PlayerAnim.Move(move);
-
-
+        _rigid.velocity = new Vector2((move1 + move) * _speed, _rigid.velocity.y);
+        _PlayerAnim.Move(move1 + move);
     }
 
     bool isGrounded()
     {
-
-        
         // collider grounded and jump
-         RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.down , 1f, 1<<8);
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.down, 1f, 1 << 8);
 
-       // RaycastHit2D hitInfo = Physics2D.CircleCast(transform.position, 1f, Vector2.down * 0.35f);
+        // RaycastHit2D hitInfo = Physics2D.CircleCast(transform.position, 1f, Vector2.down * 0.35f);
 
         if (hitInfo.collider != null)
         {
@@ -178,10 +159,8 @@ public class Player : MonoBehaviour
         if (faceRight == true)
         {
             _PlayerSprite.flipX = false;
-           Vector3 pos = _PlayerSprite.transform.localPosition;
+            Vector3 pos = _PlayerSprite.transform.localPosition;
             _PlayerSprite.transform.localPosition = pos;
-
-
         }
 
         // when the player flip (faceleft)
@@ -204,7 +183,7 @@ public class Player : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.tag == "PowerIcon")
+        if (collision.gameObject.tag == "PowerIcon")
         {
             if (_canPower)
             {
@@ -233,5 +212,4 @@ public class Player : MonoBehaviour
         _speed = 5f;
         _jumpForce = 5f;
     }
-
 }
