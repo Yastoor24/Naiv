417467bc449b5a3d29260a8f55fdc4a242458ffc
shaima mousa <shaima.mousa.alharbi@gmail.com>
2019-07-17 11:26:55 +0300

    update asset
    update assest for progress bar.

--- Naiv_game/Assets/Scripts/dialog/ShowUI.cs
+++ Naiv_game/Assets/Scripts/dialog/ShowUI.cs
@@ -1,23 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class ShowUI : MonoBehaviour
 {
 
     public GameObject Panel;
-   
+
     // Start is called before the first frame update
     void Start()
     {
         Panel.SetActive(false);
-  }
+     
+    }
 
     // Update is called once per frame
     void OnTriggerEnter2D(Collider2D p)
     {
         if (p.gameObject.tag == "Player")
         {
+
             Panel.SetActive(true);
             Time.timeScale = 0.0f;
            
