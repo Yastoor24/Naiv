14cb9a19a535aeb7bb015b00c7d3cf5c1741590c
maymonah <Maymoonah1416@gmail.com>
2019-07-18 10:52:15 +0300

    add escape button in option menu
    fix the buttons  with making new one one add  the escape putton in option which let  the player goas to  welcome menu

--- Naiv_game/Assets/Scripts/menu/Scripts/OptionMenu.cs
+++ Naiv_game/Assets/Scripts/menu/Scripts/OptionMenu.cs
@@ -1,24 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class OptionMenu : MonoBehaviour
 {
-    public OptionMenu optionMenu;
+  public static bool GameIsPaused = false;
+  public GameObject PauseMenuUI;
+  [SerializeField] string Scene;
+
     // Start is called before the first frame update
     void Start()
     {
 
     }
-
     // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            optionMenu.gameObject.SetActive(!optionMenu.gameObject.activeSelf);
+        void Update()
+      {
+        if ( Input.GetKeyDown(KeyCode.Escape)){
+          if(GameIsPaused){
+            resume();
+          } else  {
+            pause();
+          }
+         }
+      }
 
 
-        }
-    }
+      public  void resume(){
+           PauseMenuUI.SetActive(false);
+           Time.timeScale=1f;
+           GameIsPaused = false;
+           NewGameBtn(Scene);
+
+         }
+         void pause(){
+           PauseMenuUI.SetActive(true);
+           Time.timeScale=0f;
+           GameIsPaused = true;
+
+         }
+
+         public void NewGameBtn(string _newGameLevel)
+        {   SceneManager.LoadScene(_newGameLevel);  }
 }
